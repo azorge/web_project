348c5c8875558337a57523509e5bc76a0d27fdb5
azorge <vits.alex@gmail.com>
2016-11-12 01:30:12 +0300

    add gunicorn.conf, hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def application(environ, start_response):
+  status = '200 OK'
+  header = [('Content-Type', 'text/plain')]
+  query = environ['QUERY_STRING']
+  res = '\n'.join(query.split('&'))
+  start_responce(status, header)
+  return res
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#!/bin/sh
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my_project.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/my_project.conf
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-#sudo /etc/init.d/mysql start
