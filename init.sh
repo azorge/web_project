06c259c9513c31938695bd41ff7126f3e3cd9313
azorge <vits.alex@gmail.com>
2016-11-11 19:23:01 +0300

    initial commit

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start
