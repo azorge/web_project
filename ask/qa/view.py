06d4ebd3f03abdceb0b5b220a6d4d9bc866fc7b8
azorge <vits.alex@gmail.com>
2016-11-15 18:56:20 +0300

    prepare django project

---
+++ ask/qa/view.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def test(request, *args, **kwargs):
+  return HttpResponse('OK')
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my_project.conf
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/my_project.conf
+sudo ln -s /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_qa.conf /etc/gunicorn.d/qa
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
